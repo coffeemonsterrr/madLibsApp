ec5bc7b5e64e5cfc2ebd7f7b798c3e1b610c2a31
Mikearce Pascual <mikearce.pascual@gmail.com>
2022-07-02 01:19:18 +0800

    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,5 @@
+let originalStory = 'In Google, computer pioneer Mikearce found himself working on a project. Word got out that the team had "debugged" the error';
+
+let userInputs = [];
+
+console.log(`In ${userInputs[0]}, computer pioneer ${userInputs[1]} found himself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`);
