0ed1921bd45078d9c50e9cabcf0a54ebf4c030e6
Mikearce Pascual <mikearce.pascual@gmail.com>
2022-07-02 01:22:43 +0800

    Added declarations of mad-lib replacement variables

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-let originalStory = 'In Google, computer pioneer Mikearce found himself working on a project. Word got out that the team had "debugged" the error';
+let originalStory = 'In ${userInputs[0]}, computer pioneer ${userInputs[1]} found himself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}';
 
 let userInputs = [];
+
+let numberOfQuestions = 3;
 
 console.log(`In ${userInputs[0]}, computer pioneer ${userInputs[1]} found himself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`);
